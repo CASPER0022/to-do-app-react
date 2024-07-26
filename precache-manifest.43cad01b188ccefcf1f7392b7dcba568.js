d74e46d68bd778569ea48bb78c514d6e2944288a
CASPER00 <heyitsmealbinjohn@gmail.com>
2024-07-27 00:37:24 +0530

    Updates

---
+++ precache-manifest.43cad01b188ccefcf1f7392b7dcba568.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c352f38b22b6e09a1a6a297b8b96ca75",
+    "url": "/https//CASPER0022.github.io/to-do-app-react/index.html"
+  },
+  {
+    "revision": "e6af91ef568df22f8f4b",
+    "url": "/https//CASPER0022.github.io/to-do-app-react/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "0f82678edcccd7b7b3dc",
+    "url": "/https//CASPER0022.github.io/to-do-app-react/static/js/2.9e087dd0.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/https//CASPER0022.github.io/to-do-app-react/static/js/2.9e087dd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e3381193f40ac84ac51",
+    "url": "/https//CASPER0022.github.io/to-do-app-react/static/js/3.092472d1.chunk.js"
+  },
+  {
+    "revision": "e6af91ef568df22f8f4b",
+    "url": "/https//CASPER0022.github.io/to-do-app-react/static/js/main.c02f1bc5.chunk.js"
+  },
+  {
+    "revision": "14e33ba0517c810f5fea",
+    "url": "/https//CASPER0022.github.io/to-do-app-react/static/js/runtime-main.3c1afab0.js"
+  }
+]);
